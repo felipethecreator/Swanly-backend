5aa8582615e20b7ca9cf43dec22b5098af961702
Felipe Rodrigues <feliperodriguesqueiroz122@gmail.com>
2024-10-28 16:59:17 -0300

    feat: creates a server with Fiber

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
-	fmt.Print("Hello world!")
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello world")
+	})
+
+	app.Listen(":3000")
 }
