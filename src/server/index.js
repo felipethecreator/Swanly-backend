8b9b376668b4862047600a4da498ceae3b746a9b
Felipe Rodrigues <feliperodriguesqueiroz122@gmail.com>
2024-11-13 14:05:41 -0300

    feat: Move port config to .env file

--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,15 @@
 const express = require('express');
-app = express()
+const dotenv = require('dotenv');
+app = express();
+
+dotenv.config()
 
 app.get('/', (req, res) => {
     res.send("Olá mundo!");
 })
 
-app.listen(3000, () => {
+const PORT = process.env.SERVER_PORT;
+
+app.listen(PORT, () => {
     console.log("Servidor aberto na porta 3000")
 })
