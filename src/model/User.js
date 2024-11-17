7d024df0666ee4eef079ee462f960a5ff8ffb7cd
Felipe Rodrigues <feliperodriguesqueiroz122@gmail.com>
2024-11-17 17:05:58 -0300

    feat: Implements the concept of User in bd && creates table 'Usuarios'

---
+++ src/model/User.js
@@ -0,0 +1,31 @@
+const sequelize = require('../config/db.js')
+const { DataTypes } = require('sequelize');
+
+const User = sequelize.define('Usuario', {
+    user_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    username: {
+        type: DataTypes.STRING(60),
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    accountCreationDate: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+        allowNull: false
+    }
+}, {
+    timestamps: true
+});
+
+module.exports = User;
--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const sequelize = require('../config/db.js');
+const User = require('../model/User.js')
 app = express();
 
 dotenv.config()
@@ -11,6 +12,20 @@ sequelize.sync()
 }).catch(error => {
     console.log(`Deu b.o, ${error}`)
 });
+
+/* 
+ISSO NAO VAI FICAR AQUI, USE DE TESTE
+
+User.create({
+    username: "AllanAlvaresCabral",
+    email: "alvarodoisamem@hotmail.com",
+    password: "879y793189u312h9uji"
+}).then(() => {
+    console.log("criei o betinho")
+}).catch(error => {
+    console.log(`Não consegui criar o gala seca, ${error}`)
+})
+*/
 
 app.get('/', (req, res) => {
     res.send("Olá mundo!");
