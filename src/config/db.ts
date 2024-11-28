1cafeaf57962ac13dceef1c3c9b1a93557c8744c
Felipe Rodrigues <feliperodriguesqueiroz122@gmail.com>
2024-11-28 18:53:21 -0300

    fix: Included the correct variable in this archive

--- src/config/db.ts
+++ src/config/db.ts
@@ -3,7 +3,7 @@ import { DB_URI } from "./secrets";
 
 export async function connectDb() {
     try {
-        await mongoose.connect("mongodb+srv://feliperodriguesqueiroz122:7ao6YYDhLCWZOAOa@swanly.iusr6.mongodb.net/?retryWrites=true&w=majority&appName=Swanly");
+        await mongoose.connect(`${DB_URI}`);
         console.log("Conectado ao banco de dados");
     } catch (error) {
         console.log(`Ocorreu um erro: ${error}`);
