cd8d17e48f07245f47908189e644c0ca540d225f
Felipe Rodrigues <feliperodriguesqueiroz122@gmail.com>
2024-11-28 18:59:17 -0300

    feat: Implemented express user routes && express json

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,12 +1,16 @@
 import express from "express";
 import { Request, Response } from "express";
-import { DB_URI } from "../config/secrets";
 import { connectDb } from "../config/db";
+import router from "../routes/userRoutes";
 const app = express();
+
+app.use(express.json());
+app.use(router)
 
 app.get("/", (req:Request, res:Response) => {
     res.status(200).send("Hello World!")
 })
+
 
 app.listen(3000, () => {
     console.log("Servidor iniciado na porta 3000");
