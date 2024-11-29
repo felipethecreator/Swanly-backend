662e80fe31d788f2ea316b7b0f2f068a168aba07
Felipe Rodrigues <feliperodriguesqueiroz122@gmail.com>
2024-11-28 21:09:26 -0300

    feat: add better error verifications in controller

--- src/controller/userController.ts
+++ src/controller/userController.ts
@@ -1,23 +1,34 @@
 import { Request, Response } from "express";
 import { createUser } from "../service/userService";
+import { validationResult } from "express-validator";
 
 async function handleCreateUser(req: Request, res: Response): Promise<void> {
     try {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            res.status(400).json({
+                success: false,
+                message: "Erro de validação",
+                errors: errors.array(),
+            });
+            return
+        }
+
         const userData = req.body;
         await createUser(userData);
-        res.status(200).json({
-            "success": true,
-            "message": "Usuário criado com sucesso.",
-            "data": {
-                "username": userData.username,
-                "email": userData.email
-            }
+        res.status(201).json({
+            success: true,
+            message: "Usuário criado com sucesso.",
+            data: {
+                username: userData.username,
+                email: userData.email,
+            },
         });
     } catch (error) {
-        res.status(400).json({
-            "success": false,
-            "message": "Erro ao criar usuário",
-            "error": error
+        res.status(500).json({
+            success: false,
+            message: "Erro interno no servidor",
+            error: error,
         });
     }
 }
