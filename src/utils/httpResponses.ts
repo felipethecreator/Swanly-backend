2d57a5a8157ae349572a28185d833081651ca4a7
Felipe Rodrigues <feliperodriguesqueiroz122@gmail.com>
2024-12-02 13:21:20 -0300

    refactor: indented code to beauty and readable

--- src/utils/httpResponses.ts
+++ src/utils/httpResponses.ts
@@ -1,31 +1,31 @@
 import { ErrorResponse } from "../types/ErrorResponse";
 
 const errorHandler = new Map<string, ErrorResponse>([
-        ["emptyField", {            
-            success: false,
-            error: "Bad Request",
-            message: "Preencha todos os campos!"
-        }],
-        [
-         "miniumLetters", {
-            success: false,
-            error: "Bad Request",
-            message: "Seu username deve ter no mínimo 6 letras!"
-         }],
-        [
-         "maximumLetters", {
-            success: false,
-            error: "Bad Request",
-            message: "Seu username deve ter no máximo 20 letras!"
-         }
-        ],
-        [
-         "fieldWithSpace", {            
-            success: false,
-            error: "Bad Request",
-            message: "Seu username não pode conter espaços!"
-         }
-        ]
-    ]);
+   ["emptyField", {
+      success: false,
+      error: "Bad Request",
+      message: "Preencha todos os campos!"
+   }],
+   [
+      "miniumLetters", {
+         success: false,
+         error: "Bad Request",
+         message: "Seu username deve ter no mínimo 6 letras!"
+      }],
+   [
+      "maximumLetters", {
+         success: false,
+         error: "Bad Request",
+         message: "Seu username deve ter no máximo 20 letras!"
+      }
+   ],
+   [
+      "fieldWithSpace", {
+         success: false,
+         error: "Bad Request",
+         message: "Seu username não pode conter espaços!"
+      }
+   ]
+]);
 
 export default errorHandler
